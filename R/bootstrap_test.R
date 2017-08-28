5a9529abfefa39bcc108ac1579948a3a0aa5ca08
Jihui <Jihui@Jihuis-MBP.home>
2017-08-28 02:02:34 -0400

    a

--- R/bootstrap_test.R
+++ R/bootstrap_test.R
@@ -1,6 +1,6 @@
 #' Create bootstrap samples to calculate p-value
 #'
-#' @param object A formula object of the form (network object) ~ <model terms>. Model terms take the same form of ERGM terms from R package 'ergm'. 
+#' @param object A formula object of the form (network object) ~ <model terms>. Model terms take the same form of ERGM terms from R package 'ergm'.
 #' @param networks A list of observed networks. It should have a list() object.
 #' @param attr A list of vertex attributes. Default is NULL. (i.e. No attributes)
 #' @param phicoef0 Estimates of basis coefficients under H0
@@ -20,29 +20,29 @@
 #' @export
 
 bootstrap_test = function(object, networks, attr = NULL, phicoef0 = NULL, phi0 = NULL, teststat,
-                          directed = FALSE, degree.spline = 3, interior.knot = 3, 
+                          directed = FALSE, degree.spline = 3, interior.knot = 3,
                           lambda.range = seq(-3, 3, by = 0.1),
                           MCMC.burnin = 10000, MCMC.interval = 1000, NBoot = 1000, seed = 123)
 {
   set.seed(seed)
   K = length(networks)
   Bnet = bs(1:K, df = degree.spline + 1 + interior.knot, degree = degree.spline, intercept = TRUE)
-  
+
   #replacing object with current network formula
   z = deparse(object[[3]])
-  
+
   if (is.null(phi0)) {phi0 = phicoef0 %*% t(Bnet)}
-  
+
   # simulate bootstrap sample
   boot.networks = vector("list", NBoot)
-  
+
   for (s in 1:K)
   {
     num.nodes = nrow(networks[[s]])
     Bs = matrix(Bnet[s,])
-    
+
     if(is.vector(phi0) == TRUE) {coefs = as.vector(phi0[s])} else {coefs = as.vector(phi0[,s])}
-    
+
     if (is.null(attr) == FALSE)
     {
       if (is.matrix(attr[[s]]) == FALSE) {
@@ -53,53 +53,53 @@ bootstrap_test = function(object, networks, attr = NULL, phicoef0 = NULL, phi0 =
       }
       nets = network(num.nodes, vertex.attr = attrs, directed = directed)
     } else{nets = network(num.nodes, directed = directed)}
-    
+
     formula.s = as.formula(paste("nets ~ ", z, sep = ""))
-    
+
     # Use an existing function in package 'ergm'
     sims = simulate(object = formula.s, coef = coefs, nsim = NBoot, seed = seed,
                     control = control.simulate(MCMC.burnin = MCMC.burnin, MCMC.interval = MCMC.interval))
-    
+
     netarray = array(NA, dim = c(num.nodes, num.nodes, NBoot))
-    
+
     if (NBoot == 1) { boot.networks[[1]][[s]] = as.matrix.network(sims, matrix.type = "adjacency") }
-    
+
     if (NBoot > 1)
     { for (i in 1:NBoot) { boot.networks[[i]][[s]] = as.matrix(sims[[i]], matrix.type = "adjacency") } }
   }
-  
+
   boot.teststat = rep(NA, NBoot)
   for (b in 1:NBoot)
   {
 
-    net.b = boot.networks[[b]]   
-    
+    net.b = boot.networks[[b]]
+
     suppressWarnings("glm.fit: fitted probabilities numerically 0 or 1 occurred")
-    
+
     # H1
-    vcergm.false = estimate_vcergm(object = object, networks = net.b, attr = attr, 
-                                   degree.spline = degree.spline, interior.knot = interior.knot, 
+    vcergm.false = estimate_vcergm(object = object, networks = net.b, attr = attr,
+                                   degree.spline = degree.spline, interior.knot = interior.knot,
                                    directed = directed,
                                    lambda.range = lambda.range, constant = FALSE)
-    
+
     # H0 (Constant)
-    vcergm.true = estimate_vcergm(object = object, networks = net.b, attr = attr, 
-                                  degree.spline = degree.spline, interior.knot = interior.knot, 
+    vcergm.true = estimate_vcergm(object = object, networks = net.b, attr = attr,
+                                  degree.spline = degree.spline, interior.knot = interior.knot,
                                   directed = directed,
                                   lambda.range = lambda.range, constant = TRUE)
-    
-    
+
+
     phi1 = as.matrix(vcergm.false$phi.hat)
     phi0 = as.matrix(vcergm.true$phi.hat)
-    
+
     boot.teststat[b] = test_statistic(object = object, networks = net, attr = attr,
                                       phi0 = phi0, teststat = teststat[k],
-                                      degree.spline = degree.spline, interior.knot = interior.knot, 
+                                      degree.spline = degree.spline, interior.knot = interior.knot,
                                       directed = directed, NBoot = NBoot)$boot.teststat
-     
+
     cat("Calculating test statistic for bootstrap sample", b, "/", NBoot, "\n")
   }
-  
+
   pvalue = sum(teststat > boot.teststat) / NBoot
   return(list(boot.networks = boot.networks, boot.teststat = boot.teststat, pvalue = pvalue))
 }
