6a2bb82413f5376a2ebd378249af9751356e7d65
Jihui <jihui.lee94@gmail.com>
2017-09-26 18:32:04 -0400

    no message

--- R/plotting_simulation.R
+++ R/plotting_simulation.R
@@ -125,23 +125,4 @@ grid.arrange(plots, mylegend, heights = c(9/10, 1/10))
 return(Summary = Summary)
 }
 
-# Calculating quantiles  
-quan.data.frame = function(dat, stat, method) # quantiles = c(0.25, 0.5, 0.75)
-{
-  T = nrow(dat)
-  missing.indx = which(is.na(dat[,1]))
-  available.indx = setdiff(1:T, missing.indx)
-  quan.fun = function(x){quantile(as.numeric(x), probs = c(0.25, 0.5, 0.75), na.rm = TRUE)}
-  quan.value = apply(dat, 1, quan.fun)
-  out = data.frame(Q1 = quan.value[1,], Median = quan.value[2,], Q3 = quan.value[3,],
-                   Time = 1:T, Stat = rep(stat, T), Method = rep(method, T))
-  return(out)
-}
-
-# Extract legend
-g_legend = function(a.gplot) {
-  tmp = ggplot_gtable(ggplot_build(a.gplot))
-  leg = which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
-  legend = tmp$grobs[[leg]]
-  return(legend)}
 
