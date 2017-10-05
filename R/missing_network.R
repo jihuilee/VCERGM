bf9d4c5d15378813743dd932eca848a8135dd77a
Jihui <jihui.lee94@gmail.com>
2017-10-05 01:59:47 -0400

    Code optimization

--- R/missing_network.R
+++ R/missing_network.R
@@ -1,4 +1,4 @@
- #' Generate missing networks
+#' Generate missing networks
 #'
 #' @param networks A list of (fully) observed networks
 #' @param num.missing Number of missing networks. Default is NULL.
@@ -7,35 +7,35 @@
 #' @export
 
 missing_network = function(networks, num.missing = NULL, missing.time = NULL, seed = 1)
-  
+
 {
   set.seed(seed)
   K = length(networks)
-  
+
   # Make sure there is no duplicated time points.
   missing.time = unique(missing.time)
-    
-  ## If both num.missing and missing.time are specified 
+
+  ## If both num.missing and missing.time are specified
   ## 1) with different lengths, ERROR!
   ## 2) with the same length, use missing.time.
-  
+
   if (is.null(num.missing) != TRUE & is.null(missing.time) != TRUE)
   {
     if (length(missing.time) != num.missing)
-    {stop("Both num.missing and missing.time are specified and they have different lengths. Please respecify.")}    
+    {stop("Both num.missing and missing.time are specified and they have different lengths. Please respecify.")}
   }
-  
+
   if (is.null(num.missing) != TRUE & is.null(missing.time))
   {missing.time = sample(1:K, num.missing, replace = FALSE)}
-  
+
   if (is.null(missing.time) != TRUE & max(missing.time) > K)
   {
     stop("You include at least one time point beyond the actual length of observed network.
          Please respecify")
   }
-    
+
   missing.time = sort(missing.time)
-  
+
   newnetworks = list()
   for (i in 1:K)
   {
