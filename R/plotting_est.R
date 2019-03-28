c2dd4edafce5028cc6ceff0d6a3bd0a81930b48a
Jihui <jihui.lee94@gmail.com>
2019-03-28 10:35:51 -0400

    Update est plot

--- R/plotting_est.R
+++ R/plotting_est.R
@@ -2,6 +2,11 @@
 #'
 #' @param ergmest ERGM estimate object
 #' @param vcergmest VCERGM estimate object
+#' @param label Label(s) for plot title. Default is NULL (Use the label from VCERGM object).
+#' @param interval Interval size for time in x-axis. Default is 10.
+#' @param timeseq User defined timeseq. Default is NULL (Time starts from 1).
+#' @param xlab Name for x-axis. Default is "Time".
+#'
 #' @importFrom ggplot2 ggplot
 #' @importFrom ggplot2 aes
 #' @importFrom ggplot2 geom_line
@@ -18,54 +23,57 @@
 #' @importFrom gridExtra arrangeGrob
 #' @export
 
-plotting = function(ergmest, vcergmest, interval = 10, label = NULL)
+plotting = function(ergmest, vcergmest, interval = 10, xlab = "Time", label = NULL, timeseq = NULL)
 {
   ergm.phi.hat = ergmest$phi.hat
   ergm.phi.hat.smooth = ergmest$phi.hat.smooth
   vcergm.phi.hat = vcergmest$phi.hat
-  
-  tseq = 1:ncol(ergm.phi.hat) # Number of time points
+
+  # Number of time points
+  if(is.null(timeseq)){timeseq = 1:ncol(ergm.phi.hat)}
+
   nstat = nrow(ergm.phi.hat) # Number of network statistics
   stat = rownames(ergm.phi.hat) # Name of network statistics
-  
-  if (nstat > 3) {NRow = 2} else {NRow = 1}
-  
+  ntime = length(timeseq) # Number of time points
+
+#  if (nstat > 3) {NRow = 2} else {NRow = 1}
+
   plot.dat = data.frame(Value = c(c(t(ergm.phi.hat)), c(t(ergm.phi.hat.smooth)), c(t(vcergm.phi.hat))),
                         Method = rep(c("ERGM", "ERGM2", "VCERGM"),
-                                       each = nrow(ergm.phi.hat) * ncol(ergm.phi.hat)),
-                        Time = rep(1:ncol(ergm.phi.hat), nrow(ergm.phi.hat) * 3),
-                        Stat = rep(rep(stat, each = ncol(ergm.phi.hat)), 3))
-  
+                                       each = nstat * ntime),
+                        Time = rep(timeseq, nstat * 3),
+                        Stat = rep(rep(stat, each = ntime), 3))
+
   plist = vector("list", length(stat))
-  
+
   for (i in 1:nstat)
   {
     plot.dat.i = plot.dat[plot.dat$Stat == stat[i], ]
-    plist[[i]] = ggplot(data = plot.dat.i, aes(x = as.factor(Time), y = Value, col = Method, group = Method)) + 
-                  geom_line() + geom_point(size = 1) + xlab("Time") + ylab(expression(hat(phi)(t))) + 
-                  scale_x_discrete(breaks = c(1, interval * (1:floor(max(tseq)/interval)))) +
-                  ggtitle(stat[i]) + theme_bw() + theme(legend.position = "bottom")
-    
+    plist[[i]] = ggplot(data = plot.dat.i, aes(x = as.factor(Time), y = Value, col = Method, group = Method)) +
+                  geom_line() + geom_point(size = 1) + xlab("Time") + ylab(expression(hat(phi)(t))) +
+                  scale_x_discrete(breaks = c(1, interval * (1:floor(max(timeseq)/interval)))) +
+                  ggtitle(stat[i]) + theme_bw() + theme(legend.position = "bottom") + xlab(xlab)
+
     if (is.null(label) == FALSE) {plist[[i]] = plist[[i]] + ggtitle(label[i])}
   }
-  
+
   g_legend = function(a.gplot) {
     tmp = ggplot_gtable(ggplot_build(a.gplot))
     leg = which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
     legend = tmp$grobs[[leg]]
     return(legend)}
-  
+
   mylegend = g_legend(plist[[1]])
-  
-  for (i in 1:nstat) {plist[[i]] = plist[[i]] + theme(legend.position = "none") + 
+
+  for (i in 1:nstat) {plist[[i]] = plist[[i]] + theme(legend.position = "none") +
                                     theme(plot.title = element_text(hjust = 0.5))}
-  
+
   plots = do.call("arrangeGrob", c(plist, ncol = nstat))
   print(grid.arrange(plots, mylegend, heights = c(9/10, 1/10)))
 
-#  plot2 = ggplot(data = plot.dat, aes(x = as.factor(Time), y = Value, 
+#  plot2 = ggplot(data = plot.dat, aes(x = as.factor(Time), y = Value,
 #                      col = Method, group = Method), alpha = 0.8) +
-#                      geom_line() + geom_point(size = 1) + 
+#                      geom_line() + geom_point(size = 1) +
 #                      facet_grid(. ~ Stat) + xlab("Time") + ylab("Phi")
 #  print(plot2)
 }
