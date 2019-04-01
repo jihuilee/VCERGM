4d9af1c03b2574cb986b80120cd6d0a79753980c
Jihui <jihui.lee94@gmail.com>
2019-04-01 12:02:40 -0400

    Cross-sectional ERGM Inf estimates

--- R/cross_sectional_ergm.R
+++ R/cross_sectional_ergm.R
@@ -17,28 +17,28 @@
 cross_sectional_ergm = function(object, networks, attr = NULL, directed = c(TRUE, FALSE),
                                 degree.spline = 3, interior.knot = 10,
                                 lambda.range = seq(-3, 3, by = 0.1))
-  {
+{
   directed = directed[1]
-
-#  stat = unlist(strsplit(deparse(object[[3]]), " "))
-#  stat = stat[!stat %in% c("+", "=", "TRUE)", "FALSE)")]
-#  stat = stat[!stat %in% c("+", "=", "", "TRUE", "T", "T)", "TRUE)", "FALSE", "F", "F)", "FALSE)", "diff")]
-
-#  hlength = length(stat)
-
+  
+  #  stat = unlist(strsplit(deparse(object[[3]]), " "))
+  #  stat = stat[!stat %in% c("+", "=", "TRUE)", "FALSE)")]
+  #  stat = stat[!stat %in% c("+", "=", "", "TRUE", "T", "T)", "TRUE)", "FALSE", "F", "F)", "FALSE)", "diff")]
+  
+  #  hlength = length(stat)
+  
   K = length(networks)
   missing.indx = which(sapply(networks, is.null))
   available.indx = setdiff(1:K, missing.indx)
-
+  
   q = degree.spline + 1 + interior.knot
   B = bs(available.indx, df = q, degree = degree.spline, intercept = TRUE)
-
+  
   Bfull = bs(1:K, df = q, degree = degree.spline, intercept = TRUE)
-
+  
   # Cross-sectiona ERGMs
-#  phi.hat = matrix(NA, hlength, K)
+  #  phi.hat = matrix(NA, hlength, K)
   phi.hat = NULL
-
+  
   for (s in available.indx)
   {
     if (is.null(attr) == FALSE)
@@ -51,34 +51,34 @@ cross_sectional_ergm = function(object, networks, attr = NULL, directed = c(TRUE
       }
       sim.s = network(networks[[s]], vertex.attr = attr.s, directed = directed)
     } else {sim.s = network(networks[[s]], directed = directed)}
-
+    
     z = deparse(object[[3]])
-#    formula.s = as.formula(paste("sim.s ~ ", z, sep = ""))
-#    formula.s = ergm.update.formula(object, sim.s ~ ., from.new = TRUE)
+    #    formula.s = as.formula(paste("sim.s ~ ", z, sep = ""))
+    #    formula.s = ergm.update.formula(object, sim.s ~ ., from.new = TRUE)
     formula.s = nonsimp_update.formula(object, sim.s ~ ., from.new = TRUE)
-
-#    phi.hat[,s] = ergm(formula.s, estimate = "MPLE")$coef
+    
+    #    phi.hat[,s] = ergm(formula.s, estimate = "MPLE")$coef
     phi.hat = cbind(phi.hat, ergm(formula.s, estimate = "MPLE")$coef)
   }
-
+  
   # Inf
-  phi.hat[phi.hat == Inf] = abs(max(phi.hat[phi.hat != Inf])) * 10
-  phi.hat[phi.hat == -Inf] = abs(min(phi.hat[phi.hat != -Inf])) * (-10)
-  #  phi.hat[(phi.hat)^2 == Inf] = NA
+  phi.hat[(phi.hat)^2 == Inf] = NA
   
   # Smoothing
-#  phi.hat.smooth = matrix(NA, hlength, K)
+  #  phi.hat.smooth = matrix(NA, hlength, K)
   phi.hat.smooth = NULL
-
+  
   for (k in 1:nrow(phi.hat))
   {
-    smooth.coef = Pspline(y = phi.hat[k, available.indx], H = B, lambda.range = lambda.range,
-                           available.indx = available.indx, B = B, degree.spline = degree.spline)$phicoef
-#    phi.hat.smooth[k,] = Bfull %*% smooth.coef
+    available.indx.k = (available.indx)[!is.na(phi.hat[k,])]
+    B.k = bs(available.indx.k, df = q, degree = degree.spline, intercept = TRUE)
+    
+    smooth.coef = Pspline(y = phi.hat[k, available.indx.k], H = B.k, lambda.range = lambda.range,
+                          available.indx = available.indx, B = B.k, degree.spline = degree.spline)$phicoef
     phi.hat.smooth = rbind(phi.hat.smooth, Bfull %*% smooth.coef)
   }
-
-#  rownames(phi.hat) = rownames(phi.hat.smooth) = stat
-
+  
+  #  rownames(phi.hat) = rownames(phi.hat.smooth) = stat
+  
   return(list(phi.hat = phi.hat, phi.hat.smooth = phi.hat.smooth))
 }
